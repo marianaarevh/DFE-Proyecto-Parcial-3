490ae1a9f3662d3e56edb3cb84f7e12bf676ef0d
Mariana <marianaarevalo933@gmail.com>
2023-11-27 14:43:19 -0700

    p3: navbar - template +  estilos, rutas definidas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { InicioComponent } from './Pages/dashboard/inicio/inicio.component';
+import { PadreComponent } from './Pages/sales/padre/padre.component'
 
 const routes: Routes = [
-  { path: 'dashboard', component: InicioComponent }
+  { path: 'dashboard', component: InicioComponent },
+  { path: 'sales', component: PadreComponent }
 ];
 
 @NgModule({
